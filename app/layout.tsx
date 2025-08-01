0bc5eb140ad3d95dddd1aa90d7326e035502c3af
vitanngx <tannguyenviet23@gmail.com>
2025-08-02 01:18:19 +0200

    Cập nhật tít leeeee

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Yoko and Tan',
+  description: 'Quản lý danh mục đầu tư của chúng mìnhhhh!',
+  generator: 'he he',
 }
 
 export default function RootLayout({
